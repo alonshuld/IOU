c02a96ea1de1f5a4c1a49bf4daa9353c2cc9f36e
alonshuld <alonshuld@gmail.com>
2025-06-16 11:37:23 +0300

    IOU log class

---
+++ iou.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+
+
+from pydantic import BaseModel
+from typing import Dict
+
+
+"""
+This file implements an IOU log class
+"""
+
+
+class IOULog(BaseModel):
+    """
+    IOU Log class
+    """
+    name: str
+    owes: Dict[str, float]
+    owed_by: Dict[str, float]
+    balance: float
